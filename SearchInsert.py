c2c2e434ae77df5e42450e296ec6c6ab1c22b44f
AhmedShehab <ahmed4ehab@yahoo.com>
2021-08-05 12:16:08 +0200

    A program that searches for an element in an array and returns it's index or where it should be placed if not available

---
+++ SearchInsert.py
@@ -0,0 +1,26 @@
+from math import floor
+
+def searchInsert(nums, target):
+    start = 0
+    end = len (nums) - 1
+    while end > start:
+        index = floor((end + start)/ 2)
+        if nums[index] == target:
+            return index
+        elif nums[index] > target:
+            end = index
+        elif nums[index] < target:
+            start = index + 1
+
+    if end == start:
+        if nums[end] == target:
+            return end
+        elif nums[end] > target:
+            return end 
+        else:
+            return end + 1
+
+# Testing Data
+nums1 = [1,3]
+target1 = 0
+print(searchInsert(nums1,target1))
