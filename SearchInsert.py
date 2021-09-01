8d5bbf3e9acf02f203f0505c391ad554dce9d52e
AhmedShehab <ahmed4ehab@yahoo.com>
2021-09-01 12:06:11 +0200

    Length of last word

--- SearchInsert.py
+++ SearchInsert.py
@@ -1,10 +1,11 @@
 from math import floor
 
+
 def searchInsert(nums, target):
     start = 0
-    end = len (nums) - 1
+    end = len(nums) - 1
     while end > start:
-        index = floor((end + start)/ 2)
+        index = floor((end + start) / 2)
         if nums[index] == target:
             return index
         elif nums[index] > target:
@@ -16,11 +17,12 @@ def searchInsert(nums, target):
         if nums[end] == target:
             return end
         elif nums[end] > target:
-            return end 
+            return end
         else:
             return end + 1
 
+
 # Testing Data
-nums1 = [1,3]
+nums1 = [1, 3]
 target1 = 0
-print(searchInsert(nums1,target1))
+print(searchInsert(nums1, target1))
---
+++ lastWordLength.py
@@ -0,0 +1,24 @@
+def lengthOfLastWord(s: str) -> int:
+    # Pop the space element in the string if the flag is not raised
+    # Else if the string ends or a space is found Return Count
+    
+    # Declaring our variables 
+    flag = 0
+    count = 0
+    
+    for i in range(0,len(s)):
+        last_element = s[-1 * (i+1)]
+        if last_element == "" and flag == 0:
+            continue 
+            
+        elif last_element == "" and flag == 1:
+            break 
+            
+        elif last_element != "":
+            flag = 1
+            count +=1   
+            
+    return count
+
+
+print(lengthOfLastWord("Hello World"))
